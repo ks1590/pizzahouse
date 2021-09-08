87029ac637001f77695a63039cc42c62d897c82c
kensuke suzuki <35k.0101@gmail.com>
2021-09-09 00:04:12 +0900

    add:layoutフォルダ

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,15 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
+});
+
+Route::get('/pizzas', function () {
+  $pizzas = [
+    ['type' => 'hawaiian', 'base' => 'cheesy crust'],
+    ['type' => 'volcano', 'base' => 'garlic crust'],
+    ['type' => 'veg supreme', 'base' => 'thin & crispy']
+  ];
+  
+  return view('pizzas', ['pizzas' => $pizzas]);
 });
